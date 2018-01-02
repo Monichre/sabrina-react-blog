e86cbf74f55edd3a71ff8cfd956ff42b134663d3
Liam Ellis <liamhellis@gmail.com>
2018-01-02 14:50:11 -0500

    vriables javing client side trouble

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,13 @@
 import runtimeEnv from '@mars/heroku-js-runtime-env'
 const env = runtimeEnv()
 
+const react_space = env.REACT_APP_SPACE
+const token = env.REACT_APP_ACCESS_TOKEN
+
 export const config =  {
 	title: "Theresa on the Town",	
   auth: {
-    space: env.REACT_APP_SPACE,
-    accessToken: env.REACT_APP_ACCESS_TOKEN
+    space: react_space,
+    accessToken: token
   }
 }
