6ba10212efe0a5de3df2ef96dd91cab0fe575d3e
Liam Ellis <liamhellis@gmail.com>
2018-01-02 14:43:24 -0500

    added heroku runtime environ variables

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,10 @@
-// config.js
+import runtimeEnv from '@mars/heroku-js-runtime-env'
+const env = runtimeEnv()
+
 export const config =  {
-	title: "Theresa on the Town",
-	
+	title: "Theresa on the Town",	
   auth: {
-    space: process.env.REACT_APP_SPACE,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: env.REACT_APP_SPACE,
+    accessToken: env.REACT_APP_ACCESS_TOKEN
   }
 }
